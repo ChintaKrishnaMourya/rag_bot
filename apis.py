6bdd9c4e782092f1df9901220d15546a889fc993
Chinta Krishna Mourya <118909217+ChintaKrishnaMourya@users.noreply.github.com>
2023-11-14 17:32:30 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -3,7 +3,4 @@ openai_key = "sk"
 pinekey = '7d4049cf-7663-4025-bff8-a9bafaede5f4'
 pineenv = 'us-west4-gcp-free'
 
-VECTARA_CUSTOMER_ID = '3559332523'
-VECTARA_CORPUS_ID = '1'
-VECTARA_API_KEY = 'zwt_1Ccaq4QK4c2frqT4IyZcIZyUsNwkzyS5GJ3tDw'
 
